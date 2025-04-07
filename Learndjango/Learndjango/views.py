6aeefcb7384e071ab330498d3bf74700a9cda179
surojitsaha972 <sahasurojit972@gmail.com>
2025-04-07 09:13:29 +0530

    Learning Django

--- Learndjango/Learndjango/views.py
+++ Learndjango/Learndjango/views.py
@@ -1,14 +1,18 @@
 # I have created this file
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-    return HttpResponse("Hello, I am Surojit Saha")
+    # return HttpResponse("About Surojit Saha")
+    s={'name':'Surojit Saha','place':'Durgapur'}
+    return render(request, 'index.html',s)
 
 def about(request):
     return HttpResponse("About Surojit Saha")
 
 def htmlstyle(request):
-    return HttpResponse("<h1><b>About Surojit Saha</b></h1> <a href='\'>Back</a>")
+    return HttpResponse("<h1><b>About Surojit Saha</b></h1> <a href='/'>Back</a>")
 
 def openbyurl(request):
     return HttpResponse('''<a href="https://www.youtube.com/"><b>YouTube</b></a>''')
+
